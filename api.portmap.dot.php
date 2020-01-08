d67ed2c1aa730b56f4c8a2271316db13a2225a06
Namhyeon, Go <gnh1201@gmail.com>
2020-01-08 19:55:47 +0900

    Update api.portmap.dot.php

--- api.portmap.dot.php
+++ api.portmap.dot.php
@@ -84,8 +84,8 @@ $_tbl1 = exec_db_temp_start($sql);
 // netstat (windows)
 $sql = "
 select
-    substring_index(group_concat(if(a.pos_x = 2, b.term, null)), ':', 1) as address,
-    substring_index(group_concat(if(a.pos_x = 2, b.term, null)), ':', -1) as port,
+    group_concat(if(a.pos_x = 2, b.term, null)) as address,
+    group_concat(if(a.pos_x = 2, b.term, null)) as port,
     group_concat(if(a.pos_x = 4, b.term, null)) as state,
     group_concat(if(a.pos_x = 5, b.term, null)) as pid
 from $_tbl0 a left join autoget_terms b on a.term_id = b.id
