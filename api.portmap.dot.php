91fdaf9a3e17fbd71d81725cdeb748973400995b
Namhyeon, Go <gnh1201@gmail.com>
2020-01-08 17:29:35 +0900

    Update api.portmap.dot.php

--- api.portmap.dot.php
+++ api.portmap.dot.php
@@ -2,6 +2,7 @@
 loadHelper("string.utils");
 
 $format = get_requested_value("format");
+
 $device_id = get_requested_value("device_id");
 $start_dt = get_requested_value("start_dt");
 $end_dt = get_requested_value("end_dt");
@@ -17,26 +18,24 @@ if(empty($end_dt)) {
 
 if(empty($start_dt)) {
     $start_dt = get_current_datetime(array(
-        "adjust" => "-1h"
+        "adjust" => "-24 hours"
     ));
 }
 
-// set variables
-$device = false;
-$nodes = array();
-$relations = array();
-
-// get device information
+// get devices
 $bind = array(
     "id" => $device_id
 );
+
 $sql = get_bind_to_sql_select("autoget_devices", $bind);
-$rows = exec_db_fetch_all($sql, $bind);
-foreach($rows as $row) {
-    $device = $row;
-    $nodekey = sprintf("dv_%s", $row['id']);
-    $nodes[$nodekey] = $row['computer_name'];
+$row = exec_db_fetch($sql, $bind);
+
+if(array_key_empty("id", $row)) {
+    set_error("device ID could not found");
+    show_errors();
 }
+$device = $row;
+$devices[$row['id']] = $row['computer_name'];
 
 // 1: tasklist (command_id = 1)
 // 2: netstat -anof | findstr -v 127.0.0.1 | findstr -v UDP (command_id = 2)
@@ -46,80 +45,65 @@ $sql = get_bind_to_sql_select("autoget_sheets", false, array(
         array("and", array(
             array("or", array(
                 array("and", array("eq", "command_id", 1)),
-                array("and", array("in", "pos_x", array(1, 2))),
-                array("and", array("gt", "pos_y", 3))
+                array("and", array("in", "col_n", array(1, 2))),
+                array("and", array("gt", "row_n", 3))
             )),
             array("or", array(
                 array("and", array("eq", "command_id", 2)),
-                array("and", array("in", "pos_x", array(2, 4, 5))),
-                array("and", array("gt", "pos_y", 4))
+                array("and", array("in", "col_n", array(2, 4, 5))),
+                array("and", array("gt", "row_n", 4))
             )),
             array("or", array(
                 array("and", array("eq", "command_id", 4)),
-                array("and", array("in", "pos_x", array(4, 6, 7))),
-                array("and", array("gt", "pos_y", 2))
+                array("and", array("in", "col_n", array(4, 6, 7))),
+                array("and", array("gt", "row_n", 2))
             ))
         )),
         array("and", array("gte", "datetime", $start_dt)),
         array("and", array("lte", "datetime", $end_dt)),
         array("and", array("eq", "device_id", $device_id))
-    )
+    ),
+    "setpage" => 1,
+    "setlimit" => 5000000
 ));
-$_tbl0 = exec_db_temp_start($sql);
-
-$sql = "
-select
-    a.device_id as device_id,
-    a.command_id as command_id,
-    a.pos_y as pos_y,
-    a.pos_x as pos_x,
-    b.term as term
-from $_tbl0 a
-    left join autoget_terms b on a.term_id = b.id
-";
 $_tbl1 = exec_db_temp_start($sql);
 
-// process_name, address, port, state, pid
-
-// tasklist (windows)
+//_tbl2: tasklist (windows)
 $sql = "
 select 
-    group_concat(if(pos_x = 1, term, null)) as process_name,
-    group_concat(if(pos_x = 2, term, null)) as pid
-from $_tbl1
-where command_id = 1
-group by pos_y, datetime
+    group_concat(if(a.col_n = 1, b.name, null)) as process_name,
+    group_concat(if(a.col_n = 2, b.name, null)) as pid
+from $_tbl1 a left join autoget_terms b on a.term_id = b.id
+where a.command_id = 1 group by a.row_n, a.datetime
 ";
 $_tbl2 = exec_db_temp_start($sql);
 
-// netstat (windows) - IPv4, IPv6
+// _tbl3: netstat (windows) - IPv4, IPv6
 $sql = "
 select
-    substring_index(group_concat(if(pos_x = 2, term, null)), ':', 1) as address,
-    substring_index(group_concat(if(pos_x = 2, term, null)), ':', -1) as port,
-    group_concat(if(pos_x = 4, term, null)) as state,
-    group_concat(if(pos_x = 5, term, null)) as pid
-from $_tbl1
-where command_id = 2
-group by pos_y, datetime
+    substring_index(group_concat(if(a.col_n = 2, b.name, null)), ':', 1) as address,
+    substring_index(group_concat(if(a.col_n = 2, b.name, null)), ':', -1) as port,
+    group_concat(if(a.col_n = 4, b.name, null)) as state,
+    group_concat(if(a.col_n = 5, b.name, null)) as pid
+from $_tbl1 a left join autoget_terms b on a.term_id = b.id
+where a.command_id = 2 group by a.row_n, a.datetime
 ";
 $_tbl3 = exec_db_temp_start($sql);
 
-// netstat (linux) - IPv4, IPv6
+// _tbl4: netstat (linux) - IPv4, IPv6
 $sql = "
 select
-    substring_index(group_concat(if(pos_x = 2, term, null)), ':', 1) as address,
-    substring_index(group_concat(if(pos_x = 4, term, null)), ':', -1) as port,
-    group_concat(if(pos_x = 6, term, null)) as state,
-    substring_index(group_concat(if(pos_x = 7, term, null)), '/', 1) as pid,
-    substring_index(group_concat(if(pos_x = 7, term, null)), '/', -1) as process_name
-from $_tbl1
-where command_id = 4
-group by pos_y, datetime
+    substring_index(group_concat(if(a.col_n = 2, b.name, null)), ':', 1) as address,
+    substring_index(group_concat(if(a.col_n = 4, b.name, null)), ':', -1) as port,
+    group_concat(if(col_n = 6, b.name, null)) as state,
+    substring_index(group_concat(if(a.col_n = 7, b.name, null)), '/', 1) as pid,
+    substring_index(group_concat(if(a.col_n = 7, b.name, null)), '/', -1) as process_name
+from $_tbl1 a left join autoget_terms b on a.term_id = b.id
+where command_id = 4 group by a.row_n, a.datetime
 ";
 $_tbl4 = exec_db_temp_start($sql);
 
-// step 1
+// _tbl5: step 1
 $_tbl5 = "";
 if($device['platform'] == "windows") {
     $sql = "
@@ -139,12 +123,37 @@ $_tbl5 = exec_db_temp_start($sql);
 $sql = "select * from $_tbl5 group by port";
 $_tbl6 = exec_db_temp_start($sql);
 
+// format: json.datatables
+if($format == "json.datatables") {
+    $sql = "select * from $_tbl6";
+    $rows = exec_db_fetch_all($sql);
+    $_rows = array();
+    foreach($rows as $row) {
+        foreach($row as $k=>$v) {
+            if($k != "pid" && strlen($v) < 2) {
+                $row[$k] = "Unknown";
+            }
+        }
+        $row = array_merge(array("rowid" => get_hashed_text(implode(",", $row))), $row);
+        $_rows[] = $row;
+    }
+    header("Content-Type: application/json");
+    echo json_encode(array("data" => $_rows));
+    exit;
+}
+
+// make nodes
+$nodes = array();
+$relations = array();
+
+// add devices to nodes
+foreach($devices as $k=>$v) {
+    $nodes['dv_' . $k] = $v;
+}
+
 // add IPv4 to nodes
 $sql = "select address as ip from $_tbl6 group by ip";
 $rows = exec_db_fetch_all($sql);
-
-var_dump($rows);
-
 foreach($rows as $row) {
     if(strlen($row['ip']) < 2) {
         $row['ip'] = "Unknown";
@@ -196,13 +205,12 @@ foreach($rows as $row) {
 }
 
 // clean relations
-$_relations = array();
-foreach($relations as $rel) {
-    if(array_key_exists($rel[0], $nodes) && array_key_exists($rel[1], $nodes)) {
-        $_relations[] = $rel;
+for($i = 0; $i < count($relations); $i++) {
+    $rel = $relations[$i];
+    if(!(array_key_exists($rel[0], $nodes) && array_key_exists($rel[1], $nodes))) {
+        unset($relations[$i]);
     }
 }
-$relations = $_relations;
 
 // pass to view
 $data = array(
@@ -219,4 +227,3 @@ exec_db_temp_end($_tbl4);
 exec_db_temp_end($_tbl3);
 exec_db_temp_end($_tbl2);
 exec_db_temp_end($_tbl1);
-exec_db_temp_end($_tbl0);
