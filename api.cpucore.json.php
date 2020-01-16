8b6bb1dc7f4d3bda48cec51a34aad7cd04109e1a
root <root@localhost.localdomain>
2020-01-16 15:36:18 +0900

    fix

--- api.cpucore.json.php
+++ api.cpucore.json.php
@@ -4,6 +4,8 @@ $end_dt = get_requested_value("end_dt");
 $start_dt = get_requested_value("start_dt");
 $adjust = get_requested_value("adjust");
 
+$now_dt = get_requested_value("now_dt");
+
 if(empty($device_id)) {
     set_error("device_id is required");
     show_errors();
@@ -14,7 +16,7 @@ if(empty($adjust)) {
 }
 
 if(empty($end_dt)) {
-    $end_dt = get_current_datetime();
+    $end_dt = $now_dt;
 }
 
 if(empty($start_dt)) {
@@ -36,7 +38,28 @@ $sql = get_bind_to_sql_select("autoget_sheets", $bind, array(
 ));
 $_tbl0 = exec_db_temp_start($sql, $bind);
 
-$sql = "select device_id, max(b.term) as core from $_tbl0 a, autoget_terms b where a.term_id = b.id group by device_id";
+$sql = "select a.device_id as device_id, max(b.term) as core from $_tbl0 a, autoget_terms b where a.term_id = b.id group by a.device_id";
 $rows = exec_db_fetch_all($sql);
 
-var_dump($rows);
+$tablename = exec_db_table_create(array(
+    "device_id" => array("int", 11),
+    "core" => array("int", 3),
+    "basetime" => array("datetime")
+), "autoget_data_cpucore", array(
+    "setunique" => array(
+        "unique_1" => array("device_id")
+    )
+));
+
+foreach($rows as $row) {
+    $bind = array(
+        "device_id" => $device_id,
+        "core" => $row['core'],
+        "basetime" => $now_dt
+    );
+    $sql = get_bind_to_sql_insert($tablename, $bind, array(
+        "setkeys" => array("device_id")
+    ));
+    exec_db_query($sql, $bind);
+}
+
