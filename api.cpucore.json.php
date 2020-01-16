603a449f8f8f730705207299f744aa151f46bc4d
Namhyeon, Go <gnh1201@gmail.com>
2020-01-16 16:00:09 +0900

    Update api.cpucore.json.php

--- api.cpucore.json.php
+++ api.cpucore.json.php
@@ -3,6 +3,7 @@ $device_id = get_requested_value("device_id");
 $end_dt = get_requested_value("end_dt");
 $start_dt = get_requested_value("start_dt");
 $adjust = get_requested_value("adjust");
+$mode = get_requested_value("mode");
 
 $now_dt = get_requested_value("now_dt");
 
@@ -26,40 +27,64 @@ if(empty($start_dt)) {
     ));
 }
 
-$bind = array(
-    "device_id" => $device_id,
-    "command_id" => 50
+$data = array(
+    "success" => false
 );
-$sql = get_bind_to_sql_select("autoget_sheets", $bind, array(
-    "setwheres" => array(
-        array("and", array("lte", "datetime", $end_dt)),
-        array("and", array("gte", "datetime", $start_dt))
-    )
-));
-$_tbl0 = exec_db_temp_start($sql, $bind);
 
-$sql = "select a.device_id as device_id, max(b.term) as core from $_tbl0 a, autoget_terms b where a.term_id = b.id group by a.device_id";
-$rows = exec_db_fetch_all($sql);
-
-$tablename = exec_db_table_create(array(
-    "device_id" => array("int", 11),
-    "core" => array("int", 3),
-    "basetime" => array("datetime")
-), "autoget_data_cpucore", array(
-    "setunique" => array(
-        "unique_1" => array("device_id")
-    )
-));
-
-foreach($rows as $row) {
+if($mode == "background") {
     $bind = array(
         "device_id" => $device_id,
-        "core" => $row['core'],
-        "basetime" => $now_dt
+        "command_id" => 50
     );
-    $sql = get_bind_to_sql_insert($tablename, $bind, array(
-        "setkeys" => array("device_id")
+    $sql = get_bind_to_sql_select("autoget_sheets", $bind, array(
+        "setwheres" => array(
+            array("and", array("lte", "datetime", $end_dt)),
+            array("and", array("gte", "datetime", $start_dt))
+        )
     ));
-    exec_db_query($sql, $bind);
+    $_tbl0 = exec_db_temp_start($sql, $bind);
+
+    $sql = "select a.device_id as device_id, max(b.term) as core from $_tbl0 a, autoget_terms b where a.term_id = b.id group by a.device_id";
+    $rows = exec_db_fetch_all($sql);
+
+    $tablename = exec_db_table_create(array(
+        "device_id" => array("int", 11),
+        "core" => array("int", 3),
+        "basetime" => array("datetime")
+    ), "autoget_data_cpucore", array(
+        "setunique" => array(
+            "unique_1" => array("device_id")
+        )
+    ));
+    
+    foreach($rows as $row) {
+        $bind = array(
+            "device_id" => $device_id,
+            "core" => $row['core'],
+            "basetime" => $now_dt
+        );
+        $sql = get_bind_to_sql_insert($tablename, $bind, array(
+            "setkeys" => array("device_id")
+        ));
+        exec_db_query($sql, $bind);
+    }
+    
+    $data['success'] = true;
+} else {
+    $bind = array(
+        "device_id" => $device_id
+    );
+    $sql = get_bind_to_sql_select("autoget_data_cpucore", $bind, array(
+        "setwheres" => array(
+            array("and", array("lte", "basetime", $end_dt)),
+            array("and", array("gte", "basetime", $start_dt))
+        )
+    ));
+    $rows = exec_db_query($sql, $bind);
+    
+    $data['success'] = true;
+    $data['data'] = $rows;
 }
 
+header("Content-Type: application/json");
+echo json_encode($data);
