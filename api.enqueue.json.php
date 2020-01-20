35c65e6dcff047c4202afc2fdb9c2db984dead83
root <root@localhost.localdomain>
2020-01-20 14:40:26 +0900

    fix

--- api.enqueue.json.php
+++ api.enqueue.json.php
@@ -8,9 +8,9 @@ $data = array(
 // get devices
 $bind = false;
 $sql = get_bind_to_sql_select("autoget_devices", $bind);
-$rows = exec_db_fetch_all($sql, $bind);
+$devices = exec_db_fetch_all($sql, $bind);
 
-foreach($devices as $device)
+foreach($devices as $device) {
     // set variable
     $device_os = strtolower($device['os']);
     $device_id = $device['id'];
@@ -31,49 +31,51 @@ foreach($devices as $device)
 
     foreach($_rows as $_row) {
         $_pos = strpos($device_os, strtolower($_row['platform']));
-        if($_pos !== false)
-            // get the last
+        
+        // skip if invalid platform
+        if($_pos === false) continue;
+
+        // get the last
+        $__bind = array(
+            "device_id" => $device_id,
+            "command_id" => $_row['id']
+        );
+        $__sql = get_bind_to_sql_select("autoget_lasts", $__bind);
+        $__row = exec_db_fetch($__sql, $__bind);
+
+        // compare now and last
+        $last_dt = get_value_in_array("last", $__row, "");
+
+        if(!empty($last_dt)) {
             $__bind = array(
-                "device_id" => $device_id,
-                "command_id" => $_row['id']
+                "now_dt" => $now_dt,
+                "last_dt" => $last_dt
             );
-            $__sql = get_bind_to_sql_select("autoget_lasts", $__bind);
+            $__sql = sprintf("select (%s - time_to_sec(timediff(:now_dt, :last_dt))) as dtf", intval($_row['period']));
             $__row = exec_db_fetch($__sql, $__bind);
-
-            // compare now and last
-            $last_dt = get_value_in_array("last", $__row, "");
-
-            if(!empty($last_dt)) {
-                $__bind = array(
-                    "now_dt" => $now_dt,
-                    "last_dt" => $last_dt
-                );
-                $__sql = sprintf("select (%s - time_to_sec(timediff(:now_dt, :last_dt))) as dtf", intval($_row['period']));
-                $__row = exec_db_fetch($__sql, $__bind);
-                $dtf = intval(get_value_in_array("dtf", $__row, 0));
-                if($dtf > 0) {
-                    //write_common_log("## skip. dtf: " . $dtf, "api.agent.noarch");
-                    continue;
-                } else {
-                    //write_common_log("## next. dtf: " . $dtf, "api.agent.noarch");
-                }
+            $dtf = intval(get_value_in_array("dtf", $__row, 0));
+            if($dtf > 0) {
+                //write_common_log("## skip. dtf: " . $dtf, "api.agent.noarch");
+                continue;
+            } else {
+                //write_common_log("## next. dtf: " . $dtf, "api.agent.noarch");
             }
-
-            // add to queue
-            $__bind = array(
-                "device_id" => $device_id,
-                "jobkey" => "cmd",
-                "jobstage" => $_row['id'],
-                "message" => $_row['command'],
-                "created_on" => get_current_datetime(),
-                "expired_on" => get_current_datetime(array(
-                    "now" => $now_dt,
-                    "adjust" => "10m"
-                ))
-            );
-            $__sql = get_bind_to_sql_insert("autoget_tx_queue", $__bind);
-            exec_db_query($__sql, $__bind);
         }
+
+        // add to queue
+        $__bind = array(
+            "device_id" => $device_id,
+            "jobkey" => "cmd",
+            "jobstage" => $_row['id'],
+            "message" => $_row['command'],
+            "created_on" => get_current_datetime(),
+            "expired_on" => get_current_datetime(array(
+                "now" => $now_dt,
+                "adjust" => "10m"
+            ))
+        );
+        $__sql = get_bind_to_sql_insert("autoget_tx_queue", $__bind);
+        exec_db_query($__sql, $__bind);
     }
 }
 
